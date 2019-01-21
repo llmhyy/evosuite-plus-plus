9ddd4ab744a44409e49ff01b77e70126829a0ab1
Tran Thi Ly Ly <lylypp6987@gmail.com>
2019-01-21 11:12:03 +0800

    minor

--- EvosuiteTest/src/com/test/batch/ListMethodsBatch.java
+++ EvosuiteTest/src/com/test/batch/ListMethodsBatch.java
@@ -1,11 +1,7 @@
 package com.test.batch;
 
 import java.io.File;
-import java.io.FilenameFilter;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -14,17 +10,15 @@ import org.apache.commons.lang3.math.NumberUtils;
 import org.evosuite.EvoSuite;
 import org.evosuite.Properties;
 import org.evosuite.classpath.ClassPathHandler;
-import org.evosuite.utils.CollectionUtil;
 
 import com.test.EvosuiteForMethod;
-import com.test.utils.AlphanumComparator;
+import com.test.tool.SFBenchmarkUtils;
 
 public class ListMethodsBatch {
 
 	
 	public static void main(String[] args) {
-		String sF100Folder = "/Users/lylytran/Projects/Evosuite/experiments/SF100_unittest";
-		Map<String, File> projectFolders = listProjectFolders(sF100Folder);
+		Map<String, File> projectFolders = SFBenchmarkUtils.listProjectFolders();
 		for (String projectName : projectFolders.keySet()) {
 			File projectFolder = projectFolders.get(projectName);
 			String[] name = projectFolder.getName().split("_");
@@ -55,28 +49,5 @@ public class ListMethodsBatch {
 			}
 		}
 	}
-
-	public static Map<String, File> listProjectFolders(String sf100Folder) {
-		File[] files = new File(sf100Folder).listFiles(new FilenameFilter() {
-			
-			@Override
-			public boolean accept(File dir, String name) {
-				return name.contains("_") && !name.startsWith(".");
-			}
-		});
-		List<File> projectFolders = CollectionUtil.toArrayList(files);
-		AlphanumComparator comparator = new AlphanumComparator();
-		Collections.sort(projectFolders, new Comparator<File>() {
-
-			@Override
-			public int compare(File o1, File o2) {
-				return comparator.compare(o1.getName(), o2.getName());
-			}
-		});
-		Map<String, File> result = new LinkedHashMap<String, File>();
-		for (File folder : projectFolders) {
-			result.put(folder.getName().split("_")[1], folder);
-		}
-		return result;
-	}
+	
 }
