3e172e87603cfadf96bf9ec074e1b55097793dd4
Yun Lin <llmhyy@gmail.com>
2019-02-02 22:06:45 +0800

    refactoring

--- EvosuiteTest/src/evosuite/shell/experiment/EvoTestSingleMethod.java
+++ EvosuiteTest/src/evosuite/shell/experiment/EvoTestSingleMethod.java
@@ -11,10 +11,12 @@ import evosuite.shell.EvosuiteForMethod;
 import evosuite.shell.FileUtils;
 
 public class EvoTestSingleMethod {
+	
+	String fitnessAppraoch = "branch";
 
 	@Before
 	public void setup() {
-		SFConfiguration.sfBenchmarkFolder = "/Users/lylytran/Projects/Evosuite/experiments/SF100_unittest";
+		SFConfiguration.sfBenchmarkFolder = "E:\\linyun\\git_space\\SF100-clean";
 		Properties.CLIENT_ON_THREAD = true;
 		Properties.STATISTICS_BACKEND = StatisticsBackend.DEBUG;
 //		FileUtils.deleteFolder(new File("/Users/lylytran/Projects/Evosuite/experiments/SF100_unittest/evoTest-reports"));
@@ -30,11 +32,11 @@ public class EvoTestSingleMethod {
 				
 				};
 //				"com.ib.client.OrderState#equals(Ljava/lang/Object;)Z"};
-		
+		fitnessAppraoch = "fbranch";
 		for (int i = 0; i < 3; i++) {
 			FileUtils.deleteFolder(new File(FileUtils.getFilePath(SFConfiguration.sfBenchmarkFolder, projectId, "evosuite-tests")));
 			FileUtils.deleteFolder(new File(FileUtils.getFilePath(SFConfiguration.sfBenchmarkFolder, projectId, "evosuite-report")));
-			evoTestSingleMethod(projectId, projectName, targetMethods);
+			evoTestSingleMethod(projectId, projectName, targetMethods, fitnessAppraoch);
 			System.out.println("i=" + i);
 		}
 	}
@@ -48,7 +50,7 @@ public class EvoTestSingleMethod {
 					"net.kencochrane.a4j.file.FileUtil#getBrowseNodeFile(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)Ljava/io/File;"
 				};
 		
-		evoTestSingleMethod(projectId, projectName, targetMethods);
+		evoTestSingleMethod(projectId, projectName, targetMethods, fitnessAppraoch);
 	}
 	
 	@Test
@@ -61,7 +63,7 @@ public class EvoTestSingleMethod {
 				"com.jigen.XmlReader#parseJigenDocument(Ljava/io/File;)Lcom/jigen/xsd/JigenDocument;"
 		};
 		
-		evoTestSingleMethod(projectId, projectName, targetMethods);
+		evoTestSingleMethod(projectId, projectName, targetMethods, fitnessAppraoch);
 	}
 	
 	@Test
@@ -73,7 +75,7 @@ public class EvoTestSingleMethod {
 				"org.apache.axis2.transport.http.turnup.server.SimpleHttpServerConnection#writeRequest(Lorg/apache/axis2/transport/http/server/SimpleRequest;)V"
 		};
 		
-		evoTestSingleMethod(projectId, projectName, targetMethods);
+		evoTestSingleMethod(projectId, projectName, targetMethods, fitnessAppraoch);
 	}
 	
 	@Test
@@ -84,7 +86,7 @@ public class EvoTestSingleMethod {
 				"org.apache.poi.hssf.usermodel.HSSFDataFormat#getBuiltinFormat(Ljava/lang/String;)S"
 		};
 		
-		evoTestSingleMethod(projectId, projectName, targetMethods);
+		evoTestSingleMethod(projectId, projectName, targetMethods, fitnessAppraoch);
 	}
 	
 	@Test
@@ -97,7 +99,7 @@ public class EvoTestSingleMethod {
 				"br.com.jnfe.core.JNFeICMS#getVCredICMSSN()Ljava/math/BigDecimal;"
 		};
 		
-		evoTestSingleMethod(projectId, projectName, targetMethods);
+		evoTestSingleMethod(projectId, projectName, targetMethods, fitnessAppraoch);
 	}
 	
 	@Test
@@ -108,7 +110,7 @@ public class EvoTestSingleMethod {
 				"com.hf.sfm.util.Loader#getArrayResults()Ljava/lang/String;"
 		};
 		
-		evoTestSingleMethod(projectId, projectName, targetMethods);
+		evoTestSingleMethod(projectId, projectName, targetMethods, fitnessAppraoch);
 	}
 	
 	@Test
@@ -119,7 +121,7 @@ public class EvoTestSingleMethod {
 				"fr.unice.gfarce.interGraph.ModifTableStockage#separerColonne(I)Lfr/unice/gfarce/interGraph/TableStockage;"
 		};
 		
-		evoTestSingleMethod(projectId, projectName, targetMethods);
+		evoTestSingleMethod(projectId, projectName, targetMethods, fitnessAppraoch);
 	}
 
 	@Test
@@ -130,7 +132,7 @@ public class EvoTestSingleMethod {
 				"simulator.util.ParameterAttributes#valueFor(F)F"
 		};
 		
-		evoTestSingleMethod(projectId, projectName, targetMethods);
+		evoTestSingleMethod(projectId, projectName, targetMethods, fitnessAppraoch);
 	}
 	
 	@Test
@@ -141,7 +143,7 @@ public class EvoTestSingleMethod {
 				"com.momed.parser.MParserFactory#getParser()Lcom/momed/parser/MParser;"
 		};
 		
-		evoTestSingleMethod(projectId, projectName, targetMethods);
+		evoTestSingleMethod(projectId, projectName, targetMethods, fitnessAppraoch);
 	}
 	
 	@Test
@@ -153,7 +155,7 @@ public class EvoTestSingleMethod {
 				"dsachat.client.gui.SingleHeroTreeModel#getChild(Ljava/lang/Object;I)Ljava/lang/Object;"
 		};
 		
-		evoTestSingleMethod(projectId, projectName, targetMethods);
+		evoTestSingleMethod(projectId, projectName, targetMethods, fitnessAppraoch);
 	}
 	
 	@Test
@@ -164,7 +166,7 @@ public class EvoTestSingleMethod {
 				"org.databene.jdbacl.ResultSetIterator#close()V"
 		};
 		
-		evoTestSingleMethod(projectId, projectName, targetMethods);
+		evoTestSingleMethod(projectId, projectName, targetMethods, fitnessAppraoch);
 	}
 	
 	@Test
@@ -175,11 +177,11 @@ public class EvoTestSingleMethod {
 				"com.lts.util.prop.PropertiesUtil#findRef(Ljava/util/Properties;)Lcom/lts/util/prop/PropertiesUtil$PropertyRef;"
 		};
 		
-		evoTestSingleMethod(projectId, projectName, targetMethods);
+		evoTestSingleMethod(projectId, projectName, targetMethods, fitnessAppraoch);
 	}
 	
 	public void evoTestSingleMethod(String projectId, String projectName,
-			String[] targetMethods) {
+			String[] targetMethods, String fitnessAppraoch) {
 		/* configure */
 	
 		/* run */
@@ -187,7 +189,6 @@ public class EvoTestSingleMethod {
 		file.deleteOnExit();
 		SFBenchmarkUtils.writeInclusiveFile(file, false, projectName, targetMethods);
 
-		String fitnessAppraoch = "branch";
 		long seconds = 90;
 		boolean instrumentContext = true;
 		String[] args = new String[] {
@@ -204,32 +205,34 @@ public class EvoTestSingleMethod {
 ////				"-projectCP", cp, //;lib/commons-math-2.2.jar
 ////				"-setup", "bin", "lib/commons-math-2.2.jar",
 ////				"-Dtarget_method", targetMethod, 
-//				"-Dsearch_budget", String.valueOf(seconds),
-//				"-Dcriterion", fitnessAppraoch,
-//				"-Dinstrument_context", String.valueOf(instrumentContext), 
-////				"-Dinsertion_uut", "0.1",
-//				"-Dp_test_delete", "0",
-//				"-Dp_test_change", "0.9",
-//				"-Dp_test_insert", "0.1",
-////				"-Dheadless_chicken_test", "true",
-//				"-Dp_change_parameter", "0.1",
-//				"-Dlocal_search_rate", "-1",
-//				"-Dp_functional_mocking", "0",
-//				"-Dmock_if_no_generator", "false",
-//				"-Dfunctional_mocking_percent", "0",
-//				"-Dprimitive_reuse_probability", "0",
-//				"-Dmin_initial_tests", "1",
-//				"-Dmax_initial_tests", "50",
-//				"-Ddse_probability", "0",
-////				"-Dinstrument_method_calls", "true",
-//				"-Dinstrument_libraries", "true",
-//				"-Dinstrument_parent", "true",
-//				"-Dmax_length", "1",
-//				"-Dmax_size", "1",
-//				"-Dmax_attempts", "100",
+				"-Dsearch_budget", String.valueOf(seconds),
+				"-Dcriterion", fitnessAppraoch,
+				"-Dinstrument_context", String.valueOf(instrumentContext), 
+//				"-Dinsertion_uut", "0.1",
+				"-Dp_test_delete", "0",
+				"-Dp_test_change", "0.9",
+				"-Dp_test_insert", "0.1",
+//				"-Dheadless_chicken_test", "true",
+				"-Dp_change_parameter", "0.1",
+//				"-Dlocal_search_rate", "3",
+				"-Dp_functional_mocking", "0",
+				"-Dmock_if_no_generator", "false",
+				"-Dfunctional_mocking_percent", "0",
+				"-Dprimitive_reuse_probability", "0",
+				"-Dmin_initial_tests", "5",
+				"-Dmax_initial_tests", "30",
+				"-Ddse_probability", "0",
+//				"-Dinstrument_method_calls", "true",
+				"-Dinstrument_libraries", "true",
+				"-Dinstrument_parent", "true",
+				"-Dmax_length", "1",
+				"-Dmax_size", "1",
+				"-Dmax_attempts", "100",
+				"-Dassertions", "false",
+				"-Dstopping_condition", "maxgenerations",
 //				"-DTT", "true",
-////				"-Dtt_scope", "target",
-//				"-seed", "100"
+//				"-Dtt_scope", "target",
+				"-seed", "100"
 				
 		};
 		SFBenchmarkUtils.setupProjectProperties(projectId);
