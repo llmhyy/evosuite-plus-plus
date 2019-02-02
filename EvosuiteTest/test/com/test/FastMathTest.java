86709e75e52d0a8bf1dc4d6ef3d361172ff6f4c0
Yun Lin <llmhyy@gmail.com>
2019-02-02 22:09:05 +0800

    batch minor

--- EvosuiteTest/test/com/test/FastMathTest.java
+++ EvosuiteTest/test/com/test/FastMathTest.java
@@ -4,10 +4,7 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Tuple;
-
 import org.evosuite.Properties;
-import org.evosuite.Properties.StatisticsBackend;
 import org.evosuite.utils.MethodUtil;
 import org.junit.Test;
 
@@ -36,15 +33,29 @@ public class FastMathTest extends AbstractETest{
 
 		Properties.TIMEOUT = 30000;
 //		Properties.TIMELINE_INTERVAL = 3000;
+		int timeBudget = 300;
 		
 		String fitnessApproach = "fbranch";
+		List<EvoTestResult> l1 = runRepetativeTimes(targetClass, targetMethod, cp, timeBudget, fitnessApproach);
 		
-		int timeBudget = 300;
-		FastMathTest t = new FastMathTest();
-//		EvoTestResult tuple1 = t.evosuite(targetClass, targetMethod, cp, timeBudget, true, fitnessApproach);
-//		System.out.println("coverage:" + tuple1.getCoverage());
+		fitnessApproach = "branch";
+		List<EvoTestResult> l2 = runRepetativeTimes(targetClass, targetMethod, cp, timeBudget, fitnessApproach);
+		
+		System.out.println("fbranch" + ":");
+		for(EvoTestResult lu: l2){
+			System.out.println(lu.getCoverage());
+			System.out.println(lu.getProgress());
+		}
+		
+		System.out.println("branch" + ":");
+		for(EvoTestResult lu: l1){
+			System.out.println(lu.getCoverage());
+			System.out.println(lu.getProgress());
+		}
+	}
 
-		
+	private List<EvoTestResult> runRepetativeTimes(String targetClass, String targetMethod, String cp, int timeBudget, String fitnessApproach) {
+		FastMathTest t = new FastMathTest();
 		List<EvoTestResult> l = new ArrayList<>();
 		for(int i=0; i<5; i++){
 			try {
@@ -57,12 +68,7 @@ public class FastMathTest extends AbstractETest{
 			}
 		}
 		
-		for(EvoTestResult lu: l){
-			System.out.println(lu.getCoverage());
-			System.out.println(lu.getProgress());
-		}
+		return l;
 	}
-
-	
 
 }
