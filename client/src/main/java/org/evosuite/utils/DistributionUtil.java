577dae4e1804f50915e9d5f2dacf0a23155ef535
Yun Lin <llmhyy@gmail.com>
2019-03-26 01:50:18 +0800

    71: Modify Distribution Metrics
    finish code skeleton
    
    Task-Url: http://github.com/llmhyy/evosuite_progress/issues/71

--- client/src/main/java/org/evosuite/utils/DistributionUtil.java
+++ client/src/main/java/org/evosuite/utils/DistributionUtil.java
@@ -1,11 +1,14 @@
 package org.evosuite.utils;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.evosuite.coverage.branch.BranchCoverageGoal;
 import org.evosuite.coverage.branch.BranchCoverageTestFitness;
+import org.evosuite.graphs.cfg.BytecodeInstruction;
+import org.evosuite.graphs.cfg.ControlDependency;
 
 public class DistributionUtil {
 	public static Map<Integer, Double> computeBranchDistribution(Map<Integer, Integer> distributionMap, List<BranchCoverageTestFitness> branchGoals) {
@@ -31,18 +34,56 @@ public class DistributionUtil {
 
 	private static Map<Integer, Double> findSiblingDistribution(BranchCoverageGoal uncoveredGoal,
 			Map<Integer, Integer> distributionMap) {
-		// TODO Auto-generated method stub
-		return null;
+		int index = 1;
+		Map<Integer, Double> siblingDistribution = new HashMap<>();
+		int branchID = uncoveredGoal.getBranch().getActualBranchId();
+		int key = uncoveredGoal.getValue() ? -branchID : branchID;
+		siblingDistribution.put(index, (double)distributionMap.get(key));
+		
+		BytecodeInstruction ins = uncoveredGoal.getBranch().getInstruction();
+		while(ins.getControlDependencies().isEmpty()) {
+			index++;
+			boolean visitNewParent = false;
+			for(ControlDependency cd: ins.getControlDependencies()) {
+				branchID = cd.getBranch().getActualBranchId();
+				
+				if(siblingDistribution.containsKey(branchID) || siblingDistribution.containsKey(-branchID)) {
+					continue;
+				}
+				
+				key = cd.getBranchExpressionValue() ? -branchID : branchID;
+				siblingDistribution.put(index, (double)distributionMap.get(key));
+				
+				ins = cd.getBranch().getInstruction(); 
+				visitNewParent = true;
+				break;
+			}			
+			
+			if(!visitNewParent) {
+				break;
+			}
+		}
+		return siblingDistribution;
 	}
 
 	private static int getTotalTestCases(Map<Integer, Integer> distributionMap) {
-		// TODO Auto-generated method stub
-		return 0;
+		int sum = 0;
+		for(Integer key: distributionMap.keySet()) {
+			sum += distributionMap.get(key);
+		}
+		return sum;
 	}
 
 	private static List<BranchCoverageGoal> findUncoveredGoals(Map<Integer, Integer> distributionMap,
 			List<BranchCoverageTestFitness> branchGoals) {
-		// TODO Auto-generated method stub
-		return null;
+		List<BranchCoverageGoal> uncoveredGoals = new ArrayList<>();
+		for(BranchCoverageTestFitness ff: branchGoals) {
+			BranchCoverageGoal goal = ff.getBranchGoal();
+			int key = goal.getValue() ? goal.getId() : -goal.getId();
+			if(distributionMap.get(key) == 0) {
+				uncoveredGoals.add(goal);
+			}
+		}
+		return uncoveredGoals;
 	}
 }
