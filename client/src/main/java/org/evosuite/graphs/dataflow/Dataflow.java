a282545a6c67207f9271390aae431e20fa7a7b47
Yun Lin <llmhyy@gmail.com>
2020-01-01 15:10:01 +0800

    framework update

--- client/src/main/java/org/evosuite/graphs/dataflow/Dataflow.java
+++ client/src/main/java/org/evosuite/graphs/dataflow/Dataflow.java
@@ -88,10 +88,8 @@ public class Dataflow {
 
 					if (!b.isInstrumented()) {
 						List<Value> operandValues = getOperands(b.getInstruction());
-//						DepVariable parent = new DepVariable(className, "this", null, null);
 						for (Value value : operandValues) {
-							searchDependantVariables(value, cfg, node, classLoader, className, methodName, allDepVars,
-									visitedIns, null);
+							searchDependantVariables(value, cfg, allDepVars, visitedIns);
 						}
 					}
 
@@ -102,6 +100,201 @@ public class Dataflow {
 
 	}
 
+	
+	private static DepVariable parseVariable(SourceValue value, BytecodeInstruction defIns) {
+		//TODO
+		
+		return null;
+	}
+	
+	/**
+	 * 
+	 * This method collects all the variable depended by {@code value} and put them into {@code allDepVars}
+	 * {@code depVars}
+	 * 
+	 * This search algorithm should take a walk through data flows until we find static field, instance field, or parameter.
+	 * We would like to take the following usage into account:
+	 * a.m1(x1, x2).m2(x3).f
+	 * in which a can be either a static field, instance field, or parameter.
+	 * 
+	 * @param value
+	 * @param cfg
+	 * @param allDepVars
+	 * @param visitedIns
+	 * @return
+	 */
+	@SuppressWarnings("rawtypes")
+	private static void searchDependantVariables(Value value, ActualControlFlowGraph cfg, List<DepVariable> allDepVars,
+			Set<BytecodeInstruction> visitedIns) {
+		String className = cfg.getClassName();
+		String methodName = cfg.getMethodName();
+		InstrumentingClassLoader classLoader = TestGenerationContext.getInstance().getClassLoaderForSUT();
+		MethodNode node = getMethodNode(classLoader, className, methodName);
+		
+		if (value instanceof SourceValue) {
+			if (!visitedNode.contains(node)) {
+				DefUseAnalyzer defUseAnalyzer = new DefUseAnalyzer();
+				defUseAnalyzer.analyze(classLoader, node, className, methodName, node.access);
+				visitedNode.add(node);
+			}
+
+			SourceValue srcValue = (SourceValue) value;
+			for(AbstractInsnNode insNode: srcValue.insns) {
+				BytecodeInstruction defIns = convert2BytecodeInstruction(cfg, node, insNode);
+				if (visitedIns.contains(defIns)) {
+					continue;
+				}
+				visitedIns.add(defIns);
+				
+				DepVariable outputVar = parseVariable(srcValue, defIns);
+				/**
+				 * the variable is computed by values on stack
+				 */
+				if (defIns.getFrame().getStackSize() > 0) {
+					Frame frame = defIns.getFrame();
+					int stackSize = frame.getStackSize();
+					for (int i = 0; i < stackSize; i++) {
+						Value val = frame.getStack(i);
+						SourceValue sVal = (SourceValue)val;
+						for(AbstractInsnNode newDefInsNode: sVal.insns) {
+							BytecodeInstruction newDefIns = convert2BytecodeInstruction(cfg, node, newDefInsNode);
+							DepVariable inputVar = parseVariable((SourceValue)val, newDefIns);
+							
+							RelationBuilder.buildRelation(outputVar, inputVar);
+							searchDependantVariables(val, cfg, allDepVars, visitedIns);							
+						}
+					}
+				}
+				/**
+				 * the variable is computed by local variables, constant, or static field
+				 */
+				else {
+					//TODO handle load/const/get static, need to put the variale into the return list
+					if(outputVar.isParameter() || outputVar.isStaticField() || outputVar.isInstaceField()) {
+						allDepVars.add(outputVar);
+					}
+					else {
+						//keep traverse
+						Use use = DefUseFactory.makeUse(defIns);
+						// Ignore method parameter
+						List<Definition> defs = DefUsePool.getDefinitions(use);
+						for (Definition def : CollectionUtil.nullToEmpty(defs)) {
+							if (def != null) {
+								Frame frame = def.getFrame();
+								int stackSize = frame.getStackSize();
+								for (int i = 0; i < stackSize; i++) {
+									Value val = frame.getStack(i);
+									searchDependantVariables(val, cfg, allDepVars, visitedIns);
+								}
+							}
+						}
+					}
+				}
+				
+				if(defIns.isMethodCall()) {
+					//TODO recurisvely analyze the method information, need to take care the method 
+					//inside or outside of the class
+					searchInterproceduralVariables(allDepVars, visitedIns, defIns);
+				}
+				
+				if (defIns.getFrame().getStackSize() > 0) {
+					Frame frame = defIns.getFrame();
+					int stackSize = frame.getStackSize();
+					for (int i = 0; i < stackSize; i++) {
+						Value val = frame.getStack(i);
+						searchDependantVariables(val, cfg, allDepVars, visitedIns);
+					}
+				}
+			}
+			
+
+		}
+	}
+
+	private static BytecodeInstruction convert2BytecodeInstruction(ActualControlFlowGraph cfg, MethodNode node,
+			AbstractInsnNode ins) {
+		AbstractInsnNode condDefinition = (AbstractInsnNode)ins;
+		BytecodeInstruction defIns = cfg.getInstruction(node.instructions.indexOf(condDefinition));
+		return defIns;
+	}
+
+	private static DepVariable searchParent(BytecodeInstruction condDefinitionInstruction, ActualControlFlowGraph cfg,
+			MethodNode node) {
+		Value objVal = condDefinitionInstruction.getFrame().getStack(0);
+		if (objVal instanceof SourceValue) {
+			SourceValue objValue = (SourceValue) objVal;
+			AbstractInsnNode fieldDef = (AbstractInsnNode) objValue.insns.iterator().next();
+			BytecodeInstruction ins = cfg.getInstruction(node.instructions.indexOf(fieldDef));
+			
+			DepVariable parent = new DepVariable(condDefinitionInstruction.getClassName(), "this", ins, null);
+			if(ins.getFrame().getStackSize() == 0) {
+				return parent;
+			}
+			else {
+				DepVariable grandparent = new DepVariable(condDefinitionInstruction.getClassName(), ((FieldInsnNode)ins.getASMNode()).name, ins, null);
+				System.currentTimeMillis();
+				if(ins.isFieldUse()) {
+					objVal = condDefinitionInstruction.getFrame().getStack(0);
+				}
+				
+				return parent;
+			}
+		}
+		
+		return null;
+	}
+
+	@SuppressWarnings("rawtypes")
+	private static void searchInterproceduralVariables(
+			List<DepVariable> allDepVars, Set<BytecodeInstruction> visitedIns, BytecodeInstruction condBcDef) {
+		
+		InstrumentingClassLoader classLoader = TestGenerationContext.getInstance().getClassLoaderForSUT();
+		
+		String innerClass = condBcDef.getCalledMethodsClass();
+		String innerMethod = condBcDef.getCalledMethod();
+		ActualControlFlowGraph calledCfg = GraphPool.getInstance(classLoader).getActualCFG(innerClass, innerMethod);
+		MethodNode innerNode = getMethodNode(classLoader, innerClass, innerMethod);
+		if (calledCfg == null) {
+			BytecodeAnalyzer bytecodeAnalyzer = new BytecodeAnalyzer();
+			try {
+				bytecodeAnalyzer.analyze(classLoader, innerClass, innerMethod, innerNode);
+			} catch (AnalyzerException e) {
+				e.printStackTrace();
+			}
+			bytecodeAnalyzer.retrieveCFGGenerator().registerCFGs();
+			calledCfg = GraphPool.getInstance(classLoader).getActualCFG(innerClass, innerMethod);
+		}
+		FBranchDefUseAnalyzer.analyze(calledCfg.getRawGraph());
+
+		Set<BytecodeInstruction> exits = determineExitPoints(calledCfg);
+		for (BytecodeInstruction exit : exits) {
+			// control flow
+			if (!exit.getControlDependencies().isEmpty()) {
+				Set<ControlDependency> cds = exit.getControlDependencies();
+				for (ControlDependency cd : cds) {
+					BytecodeInstruction ins = cd.getBranch().getInstruction();
+					if (ins.getFrame().getStackSize() > 0) {
+						Frame frame = ins.getFrame();
+						int stackSize = frame.getStackSize();
+						for (int i = 0; i < stackSize; i++) {
+							Value val = frame.getStack(i);
+							searchDependantVariables(val, calledCfg, allDepVars, visitedIns);
+						}
+					}
+				}
+			}
+			// data flow
+			if (exit.getFrame().getStackSize() > 0) {
+				Frame frame = exit.getFrame();
+				int stackSize = frame.getStackSize();
+				for (int i = 0; i < stackSize; i++) {
+					Value val = frame.getStack(i);
+					searchDependantVariables(val, calledCfg, allDepVars, visitedIns);
+				}
+			}
+		}
+	}
+	
 	private static List<Value> getOperands(BytecodeInstruction branchInstruction) {
 		List<Value> values = new ArrayList<Value>();
 		CFGFrame frame = branchInstruction.getFrame();
@@ -149,198 +342,4 @@ public class Dataflow {
 		return true;
 	}
 
-	/**
-	 * 
-	 * This method collects all the variable depended by {@code value} to
-	 * {@code depVars}
-	 * 
-	 * @param value
-	 * @param cfg
-	 * @param node
-	 * @param classLoader
-	 * @param className
-	 * @param methodName
-	 * @param allDepVars
-	 * @param visitedIns
-	 * @return
-	 */
-	@SuppressWarnings("rawtypes")
-	private static void searchDependantVariables(Value value, ActualControlFlowGraph cfg, MethodNode node,
-			InstrumentingClassLoader classLoader, String className, String methodName, List<DepVariable> allDepVars,
-			Set<BytecodeInstruction> visitedIns, DepVariable callParent) {
-
-		if (value instanceof SourceValue) {
-
-			if (!visitedNode.contains(node)) {
-				DefUseAnalyzer defUseAnalyzer = new DefUseAnalyzer();
-				defUseAnalyzer.analyze(classLoader, node, className, methodName, node.access);
-				visitedNode.add(node);
-			}
-
-			SourceValue srcValue = (SourceValue) value;
-			for(AbstractInsnNode ins: srcValue.insns) {
-				AbstractInsnNode condDefinition = (AbstractInsnNode)ins;
-				BytecodeInstruction condDefinitionInstruction = cfg
-						.getInstruction(node.instructions.indexOf(condDefinition));
-				
-				if (visitedIns.contains(condDefinitionInstruction)) {
-					continue;
-				}
-				
-				visitedIns.add(condDefinitionInstruction);
-				if (condDefinitionInstruction.getFrame().getStackSize() > 0) {
-					Frame frame = condDefinitionInstruction.getFrame();
-					int stackSize = frame.getStackSize();
-					for (int i = 0; i < stackSize; i++) {
-						Value val = frame.getStack(i);
-						searchDependantVariables(val, cfg, node, classLoader, className, methodName, allDepVars, visitedIns, callParent);
-					}
-				}
-				
-				// ALOAD 0
-	//			if (condBcDef.loadsReferenceToThis()) {
-	////				depDefs.add(e);
-	////				condBcDef.getASMNode()
-	//			}
-				
-				// Field access, local variable or array load
-				if (condDefinitionInstruction.isUse()) {
-					// method call
-					if (condDefinitionInstruction.isMethodCall()) {
-						DepVariable newCalledParent = null;
-						if (!condDefinitionInstruction.getCalledCFG().isStaticMethod()) {
-							newCalledParent = searchParent(condDefinitionInstruction, cfg, node);
-							newCalledParent.setParent(callParent);
-						}
-						
-						involveRecursiveValueInMethod(classLoader, allDepVars, visitedIns, condDefinitionInstruction,
-								callParent);
-					}
-					// getField or getStatic
-					else if (condDefinitionInstruction.isFieldUse()) {
-						assert condDefinitionInstruction.getASMNode() instanceof FieldInsnNode;
-						FieldInsnNode fieldInsnNode = ((FieldInsnNode) condDefinitionInstruction.getASMNode());
-						String varName = fieldInsnNode.name;
-						String clazz = fieldInsnNode.owner;
-						
-						if (condDefinitionInstruction.isStaticDefUse()) {
-							DepVariable depVar = new DepVariable(clazz, varName, condDefinitionInstruction, null);
-							allDepVars.add(depVar);
-						} else {
-							DepVariable parent = searchParent(condDefinitionInstruction, cfg, node);
-							
-							DepVariable depVar = new DepVariable(clazz, varName, condDefinitionInstruction, parent);
-							allDepVars.add(depVar);
-						}
-						
-					}
-					// local variable
-					else {
-						Use use = DefUseFactory.makeUse(condDefinitionInstruction);
-						// Ignore method parameter
-						List<Definition> defs = DefUsePool.getDefinitions(use);
-						for (Definition def : CollectionUtil.nullToEmpty(defs)) {
-							if (def != null) {
-								Frame frame = def.getFrame();
-								int stackSize = frame.getStackSize();
-								for (int i = 0; i < stackSize; i++) {
-									Value val = frame.getStack(i);
-									searchDependantVariables(val, cfg, node, classLoader, className, methodName, allDepVars, visitedIns, callParent);
-								}
-							}
-						}
-					}
-				}
-			}
-			
-
-		}
-	}
-
-	private static DepVariable searchParent(BytecodeInstruction condDefinitionInstruction, ActualControlFlowGraph cfg,
-			MethodNode node) {
-		Value objVal = condDefinitionInstruction.getFrame().getStack(0);
-		if (objVal instanceof SourceValue) {
-			SourceValue objValue = (SourceValue) objVal;
-			AbstractInsnNode fieldDef = (AbstractInsnNode) objValue.insns.iterator().next();
-			BytecodeInstruction ins = cfg.getInstruction(node.instructions.indexOf(fieldDef));
-			
-			DepVariable parent = new DepVariable(condDefinitionInstruction.getClassName(), "this", ins, null);
-			if(ins.getFrame().getStackSize() == 0) {
-				return parent;
-			}
-			else {
-				DepVariable grandparent = new DepVariable(condDefinitionInstruction.getClassName(), ((FieldInsnNode)ins.getASMNode()).name, ins, null);
-				System.currentTimeMillis();
-				if(ins.isFieldUse()) {
-					objVal = condDefinitionInstruction.getFrame().getStack(0);
-				}
-				
-				return parent;
-			}
-		}
-		
-		return null;
-	}
-
-	@SuppressWarnings("rawtypes")
-	private static void involveRecursiveValueInMethod(InstrumentingClassLoader classLoader,
-			List<DepVariable> allDepVars, Set<BytecodeInstruction> visitedIns, BytecodeInstruction condBcDef,
-			DepVariable callParent) {
-		String innerClass = condBcDef.getCalledMethodsClass();
-		String innerMethod = condBcDef.getCalledMethod();
-		ActualControlFlowGraph calledCfg = GraphPool.getInstance(classLoader).getActualCFG(innerClass, innerMethod);
-		MethodNode innerNode = getMethodNode(classLoader, innerClass, innerMethod);
-		if (calledCfg == null) {
-			BytecodeAnalyzer bytecodeAnalyzer = new BytecodeAnalyzer();
-			try {
-				bytecodeAnalyzer.analyze(classLoader, innerClass, innerMethod, innerNode);
-			} catch (AnalyzerException e) {
-				e.printStackTrace();
-			}
-			bytecodeAnalyzer.retrieveCFGGenerator().registerCFGs();
-			calledCfg = GraphPool.getInstance(classLoader).getActualCFG(innerClass, innerMethod);
-		}
-		FBranchDefUseAnalyzer.analyze(calledCfg.getRawGraph());
-
-		Set<BytecodeInstruction> exits = determineExitPoints(calledCfg);
-		for (BytecodeInstruction exit : exits) {
-			// control flow
-			if (!exit.getControlDependencies().isEmpty()) {
-				Set<ControlDependency> cds = exit.getControlDependencies();
-				for (ControlDependency cd : cds) {
-					BytecodeInstruction ins = cd.getBranch().getInstruction();
-					if (ins.getFrame().getStackSize() > 0) {
-						Frame frame = ins.getFrame();
-						int stackSize = frame.getStackSize();
-						for (int i = 0; i < stackSize; i++) {
-							Value val = frame.getStack(i);
-							searchDependantVariables(val, calledCfg, innerNode, classLoader, innerClass, innerMethod, allDepVars, visitedIns, callParent);
-						}
-					}
-				}
-			}
-			// data flow
-			if (exit.getFrame().getStackSize() > 0) {
-				Frame frame = exit.getFrame();
-				int stackSize = frame.getStackSize();
-				for (int i = 0; i < stackSize; i++) {
-					Value val = frame.getStack(i);
-					searchDependantVariables(val, calledCfg, innerNode, classLoader, innerClass, innerMethod, allDepVars, visitedIns, callParent);
-				}
-			}
-		}
-	}
-
-//	@SuppressWarnings("rawtypes")
-//	private static void involveRecursiveValueOnStack(BytecodeInstruction def, ActualControlFlowGraph cfg,
-//			MethodNode node, InstrumentingClassLoader classLoader, String className, String methodName,
-//			List<DepVariable> allDepVariables, Set<BytecodeInstruction> vistiedIns, DepVariable callParent) {
-//		Frame frame = def.getFrame();
-//		int stackSize = frame.getStackSize();
-//		for (int i = 0; i < stackSize; i++) {
-//			Value val = frame.getStack(i);
-//			checkUseForInstruction(val, cfg, node, classLoader, className, methodName, allDepVariables, vistiedIns, callParent);
-//		}
-//	}
 }
