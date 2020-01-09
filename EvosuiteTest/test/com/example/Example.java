e9ee56c4a9c447a7cde6abb5479294d8d06aa0a2
Cary__Xx <e0140856@u.nus.edu>
2020-01-09 23:30:14 +0700

    Add example for constructor exception case

--- EvosuiteTest/test/com/example/Example.java
+++ EvosuiteTest/test/com/example/Example.java
@@ -6,6 +6,10 @@ public class Example {
 	static Test test1 = new Test(1, 2);
 	private int c;
 	private Helper x = new Helper();
+	
+	private int test1;
+	private int test2;
+	private Example example;
 
 //	public boolean test(int a, int b) {
 //		if (Util.isOk(a, b)) {
@@ -14,6 +18,7 @@ public class Example {
 //			}
 //		}
 
+	
 //	public boolean test(int a, int b){
 //		if(Util.isOk(a, b)){
 //			if(Util.isCornerCase(a, b)){
@@ -141,13 +146,23 @@ public class Example {
 		}
 	}
 
-	public static int test(int a, int b) throws Exception {
-//
-//		if (a < b) {
-//			if (b - a < 19000) {
-//				throw new Exception("DDD");
-//			}
-		if (a != b) {
+	public Example(int num1) {
+		if (num1 < 15000) {
+			throw new IllegalArgumentException("Constructor 1 : a needs to be larger than 10000");
+		}
+		this.test1 = num1;
+	}
+	
+	public Example(int num1, int num2) {
+		if (num1 < 10000) {
+			throw new IllegalArgumentException("Constructor 2 needs to be larger than 10000");
+		}
+		this.test1 = num1;
+		this.test2 = num2;
+	}
+	
+	public int test(int a, int b) {
+		if (example.test1 < b) {
 			rangeCheck(a, b);
 //			rangeCheck1(a, b);
 			if (b - a < 10) {
