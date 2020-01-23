89a1d5026718cd8ce4583c6f97908aadbbd37ba4
ziheng <e0140856>
2020-01-23 12:42:43 +0800

    minor

--- EvosuiteTest/test/com/test/BasicTest.java
+++ EvosuiteTest/test/com/test/BasicTest.java
@@ -125,43 +125,43 @@ public class BasicTest {
 		System.currentTimeMillis();
 	}
 	
-	@Test
-	public void testLayeredCallExample2() {
-//		Class<?> clazz = com.example.TestV1.class;
-//		Class<?> clazz = com.example.PassedExample1.class;
-//		Class<?> clazz = com.example.PassedExample2.class;
-//		Class<?> clazz = com.example.passedExamples.InevitableConstructorExample.class;
-//		Class<?> clazz = com.example.passedExamples.ConstructorAndCallExample.class;
+//	@Test
+//	public void testLayeredCallExample2() {
+////		Class<?> clazz = com.example.TestV1.class;
+////		Class<?> clazz = com.example.PassedExample1.class;
+////		Class<?> clazz = com.example.PassedExample2.class;
+////		Class<?> clazz = com.example.passedExamples.InevitableConstructorExample.class;
+////		Class<?> clazz = com.example.passedExamples.ConstructorAndCallExample.class;
+////		Class<?> clazz = com.example.passedExamples.LayeredCallExample2.class;
 //		Class<?> clazz = com.example.passedExamples.LayeredCallExample2.class;
-		Class<?> clazz = com.example.passedExamples.LayeredCallExample2.class;
-//		Class<?> clazz = com.example.PassedExampleColt.class;
-
-		String methodName = "targetM";
-//		String methodName = "test";
-//		String methodName = "mergeSortInPlace";
-		int parameterNum = 3;
-		
-		String targetClass = clazz.getCanonicalName();
-		Method method = TestUility.getTargetMethod(methodName, clazz, parameterNum);
-
-		String targetMethod = method.getName() + MethodUtil.getSignature(method);
-		String cp = "target/classes";
-
-		// Properties.LOCAL_SEARCH_RATE = 1;
-//		Properties.DEBUG = true;
-//		Properties.PORT = 8000;
-		Properties.CLIENT_ON_THREAD = true;
-		Properties.STATISTICS_BACKEND = StatisticsBackend.DEBUG;
-		Properties.BRANCH_COMPARISON_TYPES = true;
-		Properties.TIMEOUT = 10000000;
-//		Properties.TIMELINE_INTERVAL = 3000;
-		
-		String fitnessApproach = "fbranch";
-		
-		int timeBudget = 100000;
-		TestUility.evosuite(targetClass, targetMethod, cp, timeBudget, true, fitnessApproach);
-		System.currentTimeMillis();
-	}
+////		Class<?> clazz = com.example.PassedExampleColt.class;
+//
+//		String methodName = "targetM";
+////		String methodName = "test";
+////		String methodName = "mergeSortInPlace";
+//		int parameterNum = 3;
+//		
+//		String targetClass = clazz.getCanonicalName();
+//		Method method = TestUility.getTargetMethod(methodName, clazz, parameterNum);
+//
+//		String targetMethod = method.getName() + MethodUtil.getSignature(method);
+//		String cp = "target/classes";
+//
+//		// Properties.LOCAL_SEARCH_RATE = 1;
+////		Properties.DEBUG = true;
+////		Properties.PORT = 8000;
+//		Properties.CLIENT_ON_THREAD = true;
+//		Properties.STATISTICS_BACKEND = StatisticsBackend.DEBUG;
+//		Properties.BRANCH_COMPARISON_TYPES = true;
+//		Properties.TIMEOUT = 10000000;
+////		Properties.TIMELINE_INTERVAL = 3000;
+//		
+//		String fitnessApproach = "fbranch";
+//		
+//		int timeBudget = 100000;
+//		TestUility.evosuite(targetClass, targetMethod, cp, timeBudget, true, fitnessApproach);
+//		System.currentTimeMillis();
+//	}
 	
 	@Test
 	public void testMultipleConstructorsExample() {
