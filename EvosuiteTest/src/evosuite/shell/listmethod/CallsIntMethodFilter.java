180d5bc9da011ef397344e62c146293eddc66887
ongaaron <ong.aaron96@gmail.com>
2020-03-26 17:33:55 +0800

    Refactored filters to extend MethodFlagCondFilter

--- EvosuiteTest/src/evosuite/shell/listmethod/CallsIntMethodFilter.java
+++ EvosuiteTest/src/evosuite/shell/listmethod/CallsIntMethodFilter.java
@@ -1,22 +1,13 @@
 package evosuite.shell.listmethod;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
-import org.evosuite.TestGenerationContext;
-import org.evosuite.classpath.ResourceList;
 import org.evosuite.graphs.GraphPool;
 import org.evosuite.graphs.cfg.ActualControlFlowGraph;
 import org.evosuite.graphs.cfg.BytecodeAnalyzer;
 import org.evosuite.graphs.cfg.BytecodeInstruction;
 import org.evosuite.graphs.cfg.BytecodeInstructionPool;
-import org.evosuite.graphs.cfg.RawControlFlowGraph;
-import org.evosuite.utils.CommonUtility;
-import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.MethodNode;
@@ -25,68 +16,39 @@ import org.slf4j.Logger;
 
 import evosuite.shell.utils.LoggerUtils;
 
-public class CallsIntMethodFilter implements IMethodFilter {
+public class CallsIntMethodFilter extends MethodFlagCondFilter {
 	private static Logger log = LoggerUtils.getLogger(CallsIntMethodFilter.class);
-
-	@Override
-	public List<String> listTestableMethods(Class<?> targetClass, ClassLoader classLoader)
-			throws AnalyzerException, IOException {
-		InputStream is = ResourceList.getInstance(TestGenerationContext.getInstance().getClassLoaderForSUT())
-				.getClassAsStream(targetClass.getName());
-		List<String> validMethods = new ArrayList<String>();
-		
-		try {
-			ClassReader reader = new ClassReader(is);
-			ClassNode cn = new ClassNode();
-			reader.accept(cn, ClassReader.SKIP_FRAMES);
-			List<MethodNode> l = cn.methods;
-
-			for (MethodNode m : l) {
-				String methodName = CommonUtility.getMethodName(m);
-				
-				// Filter out abstract methods
-                if ((m.access & Opcodes.ACC_ABSTRACT) == Opcodes.ACC_ABSTRACT) {
-                    continue;
-                }
 	
-				if ((m.access & Opcodes.ACC_PUBLIC) == Opcodes.ACC_PUBLIC
-						|| (m.access & Opcodes.ACC_PROTECTED) == Opcodes.ACC_PROTECTED
-						|| (m.access & Opcodes.ACC_PRIVATE) == 0 /* default */ ) {
-					
-					// Get CFG
-                    String className = targetClass.getName();
-                    ActualControlFlowGraph cfg = GraphPool.getInstance(classLoader).getActualCFG(className,
-                            methodName);
-                    if (cfg == null) {
-                        BytecodeAnalyzer bytecodeAnalyzer = new BytecodeAnalyzer();
-                        bytecodeAnalyzer.analyze(classLoader, className, methodName, m);
-                        bytecodeAnalyzer.retrieveCFGGenerator().registerCFGs();
-                        cfg = GraphPool.getInstance(classLoader).getActualCFG(className, methodName);
-                    }
-                    
-                    BytecodeInstructionPool insPool = BytecodeInstructionPool.getInstance(classLoader);
-                    List<BytecodeInstruction> instructions = insPool.getAllInstructionsAtMethod(className, methodName);
-                    
-                    for (BytecodeInstruction ins : instructions) {
-                    	if (ins.isMethodCall()) {                    
-                            // Check if instruction calls methods that returns INT
-                    		String calledName = ins.getCalledMethod();
-            				String calledDesc = calledName.substring(calledName.indexOf("("));
-            				Type returnType = Type.getReturnType(calledDesc);
-            				if (returnType == Type.INT_TYPE) {
-            					validMethods.add(methodName);
-            					break;
-            				}
-                    	}
-                    }
-				} 
-			}
-		} catch (Exception e) {
-			log.info("error", e);
-		} finally {
-			is.close(); 
-		}
-		
-		return validMethods;
+	@Override
+	protected boolean checkMethod(ClassLoader classLoader, String className, String methodName, MethodNode node,
+			ClassNode cn) throws AnalyzerException, IOException, ClassNotFoundException {
+		log.debug(String.format("#Method %s#%s", className, methodName));
+
+		// Get actual CFG for target method
+        ActualControlFlowGraph cfg = GraphPool.getInstance(classLoader).getActualCFG(className,
+                methodName);
+        if (cfg == null) {
+            BytecodeAnalyzer bytecodeAnalyzer = new BytecodeAnalyzer();
+            bytecodeAnalyzer.analyze(classLoader, className, methodName, node);
+            bytecodeAnalyzer.retrieveCFGGenerator().registerCFGs();
+            cfg = GraphPool.getInstance(classLoader).getActualCFG(className, methodName);
+        }
+        
+        BytecodeInstructionPool insPool = BytecodeInstructionPool.getInstance(classLoader);
+        List<BytecodeInstruction> instructions = insPool.getAllInstructionsAtMethod(className, methodName);
+        
+        // Check if instruction calls methods that returns INT
+        for (BytecodeInstruction ins : instructions) {
+        	if (ins.isMethodCall()) {                    
+        		String calledName = ins.getCalledMethod();
+				String calledDesc = calledName.substring(calledName.indexOf("("));
+				Type returnType = Type.getReturnType(calledDesc);
+				if (returnType == Type.INT_TYPE) {
+					return true;
+				}
+        	}
+        }
+
+		return false;
 	}
 }
--- EvosuiteTest/src/evosuite/shell/listmethod/CallsRecursiveMethodFilter.java
+++ EvosuiteTest/src/evosuite/shell/listmethod/CallsRecursiveMethodFilter.java
@@ -1,22 +1,15 @@
 package evosuite.shell.listmethod;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-import org.evosuite.TestGenerationContext;
-import org.evosuite.classpath.ResourceList;
+
 import org.evosuite.graphs.GraphPool;
 import org.evosuite.graphs.cfg.ActualControlFlowGraph;
 import org.evosuite.graphs.cfg.BytecodeAnalyzer;
 import org.evosuite.graphs.cfg.BytecodeInstruction;
 import org.evosuite.graphs.cfg.BytecodeInstructionPool;
 import org.evosuite.graphs.cfg.RawControlFlowGraph;
-import org.evosuite.utils.CommonUtility;
-import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.MethodNode;
 import org.objectweb.asm.tree.analysis.AnalyzerException;
@@ -24,86 +17,53 @@ import org.slf4j.Logger;
 
 import evosuite.shell.utils.LoggerUtils;
 
-public class CallsRecursiveMethodFilter implements IMethodFilter {
+public class CallsRecursiveMethodFilter extends MethodFlagCondFilter {
 	private static Logger log = LoggerUtils.getLogger(CallsRecursiveMethodFilter.class);
 
 	@Override
-	public List<String> listTestableMethods(Class<?> targetClass, ClassLoader classLoader)
-			throws AnalyzerException, IOException {
-		InputStream is = ResourceList.getInstance(TestGenerationContext.getInstance().getClassLoaderForSUT())
-				.getClassAsStream(targetClass.getName());
-		List<String> validMethods = new ArrayList<String>();
+	protected boolean checkMethod(ClassLoader classLoader, String className, String methodName, MethodNode node,
+			ClassNode cn) throws AnalyzerException, IOException, ClassNotFoundException {
+		log.debug(String.format("#Method %s#%s", className, methodName));
 
-		try {
-			ClassReader reader = new ClassReader(is);
-			ClassNode cn = new ClassNode();
-			reader.accept(cn, ClassReader.SKIP_FRAMES);
-			List<MethodNode> l = cn.methods;
+		// Get actual CFG for target method
+        ActualControlFlowGraph cfg = GraphPool.getInstance(classLoader).getActualCFG(className,
+                methodName);
+        if (cfg == null) {
+            BytecodeAnalyzer bytecodeAnalyzer = new BytecodeAnalyzer();
+            bytecodeAnalyzer.analyze(classLoader, className, methodName, node);
+            bytecodeAnalyzer.retrieveCFGGenerator().registerCFGs();
+            cfg = GraphPool.getInstance(classLoader).getActualCFG(className, methodName);
+        }
+        
+        BytecodeInstructionPool insPool = BytecodeInstructionPool.getInstance(classLoader);
+        List<BytecodeInstruction> instructions = insPool.getAllInstructionsAtMethod(className, methodName);
+        
+        // Check if instruction calls recursive methods
+        for (BytecodeInstruction ins : instructions) {
+        	if (ins.isMethodCall()) {                    
+        		RawControlFlowGraph calledCfg = ins.getCalledCFG();
+        		if (calledCfg == null) {
+        			System.currentTimeMillis();
+        			continue;
+        		}
+        		
+        		String calledName = calledCfg.getMethodName();
+        		String calledClass = calledCfg.getClassName();
+        		Set<BytecodeInstruction> calledInstructions = calledCfg.vertexSet();
+        		for (BytecodeInstruction calledIns : calledInstructions) {
+        			if (calledIns.isMethodCall()) {
+        				if (calledName.equals(calledIns.getCalledMethod()) &&
+        						calledClass.equals(calledIns.getCalledMethodsClass())) {
+//        					System.out.println(calledName);
+//        					System.out.println(calledClass);
 
-			for (MethodNode m : l) {
-				String methodName = CommonUtility.getMethodName(m);
-				
-				// Filter out abstract methods
-                if ((m.access & Opcodes.ACC_ABSTRACT) == Opcodes.ACC_ABSTRACT) {
-                    continue;
-                }
-	
-				if ((m.access & Opcodes.ACC_PUBLIC) == Opcodes.ACC_PUBLIC
-						|| (m.access & Opcodes.ACC_PROTECTED) == Opcodes.ACC_PROTECTED
-						|| (m.access & Opcodes.ACC_PRIVATE) == 0 /* default */ ) {
-					
-					// Get CFG
-                    String className = targetClass.getName();
-                    ActualControlFlowGraph cfg = GraphPool.getInstance(classLoader).getActualCFG(className,
-                            methodName);
-                    if (cfg == null) {
-                        BytecodeAnalyzer bytecodeAnalyzer = new BytecodeAnalyzer();
-                        bytecodeAnalyzer.analyze(classLoader, className, methodName, m);
-                        bytecodeAnalyzer.retrieveCFGGenerator().registerCFGs();
-                        cfg = GraphPool.getInstance(classLoader).getActualCFG(className, methodName);
-                    }
-                    
-                    BytecodeInstructionPool insPool = BytecodeInstructionPool.getInstance(classLoader);
-                    List<BytecodeInstruction> instructions = insPool.getAllInstructionsAtMethod(className, methodName);
-                    for (BytecodeInstruction ins : instructions) {
-                    	if (ins.isMethodCall()) {                    
-                            // Check if this called method is recursive
-                    		RawControlFlowGraph calledCfg = ins.getCalledCFG();
-                    		if (calledCfg == null) {
-                    			continue;
-                    		}
-                    		
-                    		Boolean isRecursive = false;
-                    		String calledName = calledCfg.getMethodName();
-                    		String calledClass = calledCfg.getClassName();
-                    		Set<BytecodeInstruction> calledInstructions = calledCfg.vertexSet();
-                    		for (BytecodeInstruction calledIns : calledInstructions) {
-                    			if (calledIns.isMethodCall()) {
-                    				if (calledName.equals(calledIns.getCalledMethod()) &&
-                    						calledClass.equals(calledIns.getCalledMethodsClass())) {
-                    					
-                    					System.out.println(calledName);
-                    					System.out.println(calledClass);
-                    					isRecursive = true;
-                    					break;
-                    				}
-                    			}
-                    		}
-                    		
-                    		if (isRecursive) {
-                    			validMethods.add(methodName);
-            					break;
-                    		}
-                    	}
-                    }
-				} 
-			}
-		} catch (Exception e) {
-			log.info("error", e);
-		} finally {
-			is.close(); 
-		}
-		
-		return validMethods;
+        					return true;
+        				}
+        			}
+        		}
+        	}
+        }
+
+		return false;
 	}
 }
