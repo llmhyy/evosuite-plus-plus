ef328d78faa99a9e0b44e8cfaeace4dbe9a7030c
Yun Lin <llmhyy@gmail.com>
2020-06-10 00:02:46 +0800

    add an example to test static public field

---
+++ EvosuiteTest/test/regression/objectconstruction/example/staticfield/StaticFieldExample.java
@@ -0,0 +1,32 @@
+package regression.objectconstruction.example.staticfield;
+
+/**
+ * this example is to test when the computation graph work well to generate static field. 
+ * @author Yun Lin
+ *
+ */
+public class StaticFieldExample {
+	
+	private String name;
+	
+	public StaticFieldExample(String name) {
+		super();
+		this.setName(name);
+	}
+
+	public void method(){
+		if(Config.num > 1000){
+			if(Config.object.getName().endsWith("test")){
+				System.currentTimeMillis();
+			}
+		}
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
