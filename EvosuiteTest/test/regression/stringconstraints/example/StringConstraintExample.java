309a66d6128a12dfb82c5ecf5e919a1720862dfc
Yun Lin <llmhyy@gmail.com>
2020-06-13 02:37:30 +0800

    new test case for string constraints

---
+++ EvosuiteTest/test/regression/stringconstraints/example/StringConstraintExample.java
@@ -0,0 +1,22 @@
+package regression.stringconstraints.example;
+
+public class StringConstraintExample {
+
+	public int method(String var0, String var1, String var2) {
+		if (var0.length() == 100) {
+			System.currentTimeMillis();
+		} else
+			return 0;
+		
+		String tmp  = var0.replace(var1, "abc");
+		if (var2.equals(tmp)) {
+			System.currentTimeMillis();
+		} else
+			return 0;
+		if (var2.endsWith(var1)) {
+			return 1;
+		} else
+			return 0;
+	}
+
+}
