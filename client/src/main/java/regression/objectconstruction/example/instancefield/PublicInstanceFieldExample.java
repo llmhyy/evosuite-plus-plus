a1449f5301872d19e7b0cabe42013b98b08fd669
Cary__Xx <e0140856@u.nus.edu>
2020-06-14 16:09:08 +0800

    fix

--- client/src/main/java/regression/objectconstruction/example/instancefield/PublicInstanceFieldExample.java
+++ client/src/main/java/regression/objectconstruction/example/instancefield/PublicInstanceFieldExample.java
@@ -1,16 +1,14 @@
 package regression.objectconstruction.example.instancefield;
 
-import com.example.Student;
-
 /**
- * Test the computation graph of public field generation. 
+ * Test the computation graph of public field generation.
  *
  */
 public class PublicInstanceFieldExample {
 	public Student student1;
-	
-	public void method(int index){
-		if(student1.getAge() < 18){
+
+	public void method(int index) {
+		if (student1.getAge() < 18) {
 			System.currentTimeMillis();
 		}
 	}
