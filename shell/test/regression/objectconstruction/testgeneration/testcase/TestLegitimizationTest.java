abb353bb0bacab63786fc679c463d3f6fa5e641c
Yun Lin <llmhyy@gmail.com>
2020-08-24 04:42:02 +0800

    mionr

--- shell/test/regression/objectconstruction/testgeneration/testcase/TestLegitimizationTest.java
+++ shell/test/regression/objectconstruction/testgeneration/testcase/TestLegitimizationTest.java
@@ -510,63 +510,63 @@ public class TestLegitimizationTest extends ObjectOrientedTest {
 		
 	}
 
-	@Test
-	public void testLegitimization9() throws ClassNotFoundException, RuntimeException {
-		setup();
-
-		Properties.TARGET_CLASS = FTPSender.class.getCanonicalName();
-
-		Method method = TestUtility.getTargetMethod("execute", FTPSender.class, 2);
-		String targetMethod = method.getName() + MethodUtil.getSignature(method);
-
-		Properties.TARGET_METHOD = targetMethod;
-
-		ClassPathHandler.getInstance().changeTargetCPtoTheSameAsEvoSuite();
-		String cp0 = ClassPathHandler.getInstance().getTargetProjectClasspath();
-
-		Properties.APPLY_OBJECT_RULE = true;
-		DependencyAnalysis.analyzeClass(Properties.TARGET_CLASS, Arrays.asList(cp0.split(File.pathSeparator)));
-
-		Map<Branch, Set<DepVariable>> interestedBranches = Dataflow.branchDepVarsMap.get(Properties.TARGET_METHOD);
-		ArrayList<Branch> rankedList = new ArrayList<>(interestedBranches.keySet());
-		Collections.sort(rankedList, new Comparator<Branch>() {
-			@Override
-			public int compare(Branch o1, Branch o2) {
-				return o1.getInstruction().getLineNumber() - o2.getInstruction().getLineNumber();
-			}
-		});
-
-//			Branch b = Randomness.choice(interestedBranches.keySet());
-		Branch b = rankedList.get(0);
-
-		TestFactory testFactory = TestFactory.getInstance();
-		TestCase test = new DefaultTestCase();
-		while (test.size() == 0) {
-			testFactory.insertRandomStatement(test, 0);
-		}
-		try {
-			ConstructionPathSynthesizer cpSynthesizer = new ConstructionPathSynthesizer(testFactory);
-			cpSynthesizer.constructDifficultObjectStatement(test, b);
-
-			PartialGraph graph = cpSynthesizer.getPartialGraph();
-			Map<DepVariable, List<VariableReference>> graph2CodeMap = cpSynthesizer.getGraph2CodeMap();
-
-			TestChromosome chromosome = TestCaseLegitimizer.getInstance().legitimize(test, graph, graph2CodeMap);
-			test = chromosome.getTestCase();
-			System.out.println(test);
-
-			MethodStatement targetStatement = test.findTargetMethodCallStatement();
-			ExecutionResult result = TestCaseExecutor.runTest(test);
-			int numExecuted = result.getExecutedStatements();
-			int legitimacyDistance = targetStatement.getPosition() - numExecuted + 1;
-
-			assert legitimacyDistance == 0;
-		} catch (Exception e) {
-			e.printStackTrace();
-			assert false;
-		}
-		
-	}
+//	@Test
+//	public void testLegitimization9() throws ClassNotFoundException, RuntimeException {
+//		setup();
+//
+//		Properties.TARGET_CLASS = FTPSender.class.getCanonicalName();
+//
+//		Method method = TestUtility.getTargetMethod("execute", FTPSender.class, 2);
+//		String targetMethod = method.getName() + MethodUtil.getSignature(method);
+//
+//		Properties.TARGET_METHOD = targetMethod;
+//
+//		ClassPathHandler.getInstance().changeTargetCPtoTheSameAsEvoSuite();
+//		String cp0 = ClassPathHandler.getInstance().getTargetProjectClasspath();
+//
+//		Properties.APPLY_OBJECT_RULE = true;
+//		DependencyAnalysis.analyzeClass(Properties.TARGET_CLASS, Arrays.asList(cp0.split(File.pathSeparator)));
+//
+//		Map<Branch, Set<DepVariable>> interestedBranches = Dataflow.branchDepVarsMap.get(Properties.TARGET_METHOD);
+//		ArrayList<Branch> rankedList = new ArrayList<>(interestedBranches.keySet());
+//		Collections.sort(rankedList, new Comparator<Branch>() {
+//			@Override
+//			public int compare(Branch o1, Branch o2) {
+//				return o1.getInstruction().getLineNumber() - o2.getInstruction().getLineNumber();
+//			}
+//		});
+//
+////			Branch b = Randomness.choice(interestedBranches.keySet());
+//		Branch b = rankedList.get(0);
+//
+//		TestFactory testFactory = TestFactory.getInstance();
+//		TestCase test = new DefaultTestCase();
+//		while (test.size() == 0) {
+//			testFactory.insertRandomStatement(test, 0);
+//		}
+//		try {
+//			ConstructionPathSynthesizer cpSynthesizer = new ConstructionPathSynthesizer(testFactory);
+//			cpSynthesizer.constructDifficultObjectStatement(test, b);
+//
+//			PartialGraph graph = cpSynthesizer.getPartialGraph();
+//			Map<DepVariable, List<VariableReference>> graph2CodeMap = cpSynthesizer.getGraph2CodeMap();
+//
+//			TestChromosome chromosome = TestCaseLegitimizer.getInstance().legitimize(test, graph, graph2CodeMap);
+//			test = chromosome.getTestCase();
+//			System.out.println(test);
+//
+//			MethodStatement targetStatement = test.findTargetMethodCallStatement();
+//			ExecutionResult result = TestCaseExecutor.runTest(test);
+//			int numExecuted = result.getExecutedStatements();
+//			int legitimacyDistance = targetStatement.getPosition() - numExecuted + 1;
+//
+//			assert legitimacyDistance == 0;
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			assert false;
+//		}
+//		
+//	}
 
 	@Test
 	public void testLegitimization10() throws ClassNotFoundException, RuntimeException {
