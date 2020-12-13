6f1b4753c4f91a5f8bd5c091e150a0f300456200
Yun Lin <llmhyy@gmail.com>
2020-12-14 01:22:20 +0800

    minor

--- client/src/main/java/org/evosuite/result/seedexpr/EventSequence.java
+++ client/src/main/java/org/evosuite/result/seedexpr/EventSequence.java
@@ -2,7 +2,11 @@ package org.evosuite.result.seedexpr;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
+import org.evosuite.TestGenerationContext;
+import org.evosuite.coverage.branch.Branch;
+import org.evosuite.coverage.branch.BranchPool;
 import org.evosuite.testcase.TestChromosome;
 
 public class EventSequence {
@@ -29,9 +33,25 @@ public class EventSequence {
 			TestChromosome o = (TestChromosome)offspring;
 			TestChromosome p = (TestChromosome)parent;
 			
-			o.getLastExecutionResult().getTrace().getCoveredTrueBranches();
+			Set<Integer> newTrueBranches = o.getLastExecutionResult().getTrace().getCoveredTrueBranches();
+			Set<Integer> oldTrueBranches = p.getLastExecutionResult().getTrace().getCoveredTrueBranches();
+			Set<Integer> diffTrueBranches = diff(oldTrueBranches, newTrueBranches);
 			
+			Set<Integer> newFalseBranches = o.getLastExecutionResult().getTrace().getCoveredFalseBranches();
+			Set<Integer> oldFalseBranches = p.getLastExecutionResult().getTrace().getCoveredFalseBranches();
+			Set<Integer> diffFalseBranches = diff(oldFalseBranches, newFalseBranches);
+			
+			for(Integer branchId: diffTrueBranches) {
+				
+				
+				Branch b = BranchPool.getInstance(TestGenerationContext.getInstance().getClassLoaderForSUT()).getBranch(branchId);				
+			}
 		}
+		return null;
+	}
+
+	private static Set<Integer> diff(Set<Integer> oldTrueBranches, Set<Integer> newTrueBranches) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 }
