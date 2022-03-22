fbd27e193afa9274577e78f8246944bab0c0d8c5
Darien Chong Yung Han <e0176647@u.nus.edu>
2022-03-22 17:18:13 +0800

    Update Operation.java

--- client/src/main/java/org/evosuite/testcase/synthesizer/improvedsynth/Operation.java
+++ client/src/main/java/org/evosuite/testcase/synthesizer/improvedsynth/Operation.java
@@ -1,4 +1,7 @@
 package org.evosuite.testcase.synthesizer.improvedsynth;
 
+/**
+ * Represents an abstract operation (one of a method call or a field access).
+ */
 public abstract class Operation {
 }
