84d518cf4c78b125cbf9f32f245f781975b6a4e5
Darien Chong Yung Han <e0176647@u.nus.edu>
2022-04-20 12:52:31 +0800

    Remove outdated comment

--- shell/src/test/java/testcode/graphgeneration/GenerationTest.java
+++ shell/src/test/java/testcode/graphgeneration/GenerationTest.java
@@ -61,14 +61,4 @@ public class GenerationTest {
 		graph.visualize(1000, "graph", "graph");
 		graph.transformToCode();
 	}
-	/**
-	 * Issues noted: 
-	 *   1) Some graphs (e.g. -2220534647818254570L) have multiple root nodes, 
-	 *      might need to generate a parent class to hold all root nodes. Also need to refine
-	 *      choice of target class for MUT generation.
-	 *   2) Recursive case (class has itself as a field) may trigger infinite recursion
-	 *      e.g. 4680529636307580942L, may need to provide a different kind of initialisation for these cases?
-	 *   3) Extension of recursive case (e.g. ClassX has a field of ClassY, ClassY has a field of ClassX) may
-	 *      trigger infinite recursion too (e.g. -2493682970431517121L), may need to provide a different kind of initialisation for these cases?
-	 */
 }
