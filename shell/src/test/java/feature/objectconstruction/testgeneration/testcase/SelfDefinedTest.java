df48b1c5198ffe263c20a69fd8f52c4192531a4c
Caiyi34777 <e0376971@u.nus.edu>
2023-03-05 10:15:22 +0800

    Create SelfDefinedTest.java

---
+++ shell/src/test/java/feature/objectconstruction/testgeneration/testcase/SelfDefinedTest.java
@@ -0,0 +1,298 @@
+package feature.objectconstruction.testgeneration.testcase;
+
+import common.TestUtility;
+// import feature.objectconstruction.testgeneration.example.graphcontruction.BasicRules.checkRules.BasicRules;
+import feature.objectconstruction.testgeneration.example.graphcontruction.SelfDefinedClasses.Set1.Administrator;
+
+// /Users/xucaiyi/Documents/GitHub/evosuite-plus-plus/shell/target/test-classes/feature/
+// objectconstruction/testgeneration/example/graphcontruction/SelfDefinedClasses/Set1/
+import org.evosuite.Properties;
+import org.evosuite.Properties.StatisticsBackend;
+import org.evosuite.utils.MethodUtil;
+import org.junit.Test;
+
+import java.io.File;
+import java.lang.reflect.Method;
+
+public class SelfDefinedTest extends TestUtility{
+
+//	@Before
+//	public void beforeTest() {
+//		Properties.CLIENT_ON_THREAD = true;
+//		Properties.STATISTICS_BACKEND = StatisticsBackend.DEBUG;
+//
+//		Properties.ENABLE_BRANCH_ENHANCEMENT = false;
+//		Properties.APPLY_OBJECT_RULE = true;
+//		Properties.APPLY_INTERPROCEDURAL_GRAPH_ANALYSIS = true;
+//		Properties.ADOPT_SMART_MUTATION = false;
+//
+//		Properties.INSTRUMENT_CONTEXT = true;
+//		Properties.CHROMOSOME_LENGTH = 200;
+//
+////		Properties.INDIVIDUAL_LEGITIMIZATION_BUDGET = 20;
+////		Properties.TOTAL_LEGITIMIZATION_BUDGET = 50;
+//		Properties.INDIVIDUAL_LEGITIMIZATION_BUDGET = 0;
+//		Properties.TOTAL_LEGITIMIZATION_BUDGET = 0;
+//		Properties.TIMEOUT = 10;
+////		Properties.SANDBOX_MODE = Sandbox.SandboxMode.OFF;
+//	}
+
+    @Test
+    public void testSet1() {
+        Class<?> clazz = Administrator.class;
+        String methodName = "getGrade";
+        int parameterNum = 2;
+
+        String targetClass = clazz.getCanonicalName();
+//		Method method = clazz.getMethods()[0];
+        Method method = TestUtility.getTargetMethod(methodName, clazz, parameterNum);
+
+        String targetMethod = method.getName() + MethodUtil.getSignature(method);
+        String cp = "target/test-classes" + File.pathSeparator + "target/classes";
+
+        // Properties.LOCAL_SEARCH_RATE = 1;
+//		Properties.DEBUG = true;
+//		Properties.PORT = 8000;
+        Properties.CLIENT_ON_THREAD = true;
+        Properties.STATISTICS_BACKEND = StatisticsBackend.DEBUG;
+
+        Properties.TIMEOUT = 100;
+        Properties.INDIVIDUAL_LEGITIMIZATION_BUDGET = 10;
+//		Properties.TIMELINE_INTERVAL = 3000;
+
+        String fitnessApproach = "branch";
+
+        int timeBudget = 100;
+
+        boolean aor = true;
+        double coverage = TestUtility.evoTestSingleMethod(targetClass,
+                targetMethod, timeBudget, true, aor, cp, fitnessApproach,
+                "generateMOSuite", "MOSUITE", "DynaMOSA");
+
+        System.out.println("coverage is:" + coverage);
+        assert coverage > 0.1;
+
+    }
+
+    /*
+    @Test
+    public void testBasicRulesObj() {
+        Class<?> clazz = BasicRules.class;
+        String methodName = "checkRules";
+        int parameterNum = 2;
+
+        String targetClass = clazz.getCanonicalName();
+//		Method method = clazz.getMethods()[0];
+        Method method = TestUtility.getTargetMethod(methodName, clazz, parameterNum);
+
+        String targetMethod = method.getName() + MethodUtil.getSignature(method);
+        String cp = "target/test-classes" + File.pathSeparator + "target/classes";
+
+        // Properties.LOCAL_SEARCH_RATE = 1;
+//		Properties.DEBUG = true;
+//		Properties.PORT = 8000;
+        Properties.CLIENT_ON_THREAD = true;
+        Properties.STATISTICS_BACKEND = StatisticsBackend.DEBUG;
+
+        Properties.TIMEOUT = 100;
+        Properties.INDIVIDUAL_LEGITIMIZATION_BUDGET = 10;
+//		Properties.TIMELINE_INTERVAL = 3000;
+
+        String fitnessApproach = "branch";
+
+        int timeBudget = 100;
+
+        boolean aor = true;
+        double coverage = TestUtility.evoTestSingleMethod(targetClass,
+                targetMethod, timeBudget, true, aor, cp, fitnessApproach,
+                "generateMOSuite", "MOSUITE", "DynaMOSA");
+
+        System.out.println("coverage is:" + coverage);
+        assert coverage > 0.1;
+
+    }
+
+    @Test
+    public void testBasicRulesSuite() {
+        Class<?> clazz = BasicRules.class;
+        String methodName = "checkRules";
+        int parameterNum = 2;
+
+        String targetClass = clazz.getCanonicalName();
+//		Method method = clazz.getMethods()[0];
+        Method method = TestUtility.getTargetMethod(methodName, clazz, parameterNum);
+
+        String targetMethod = method.getName() + MethodUtil.getSignature(method);
+        String cp = "target/test-classes" + File.pathSeparator + "target/classes";
+
+        // Properties.LOCAL_SEARCH_RATE = 1;
+//		Properties.DEBUG = true;
+//		Properties.PORT = 8000;
+        Properties.CLIENT_ON_THREAD = true;
+        Properties.STATISTICS_BACKEND = StatisticsBackend.DEBUG;
+
+        Properties.TIMEOUT = 100;
+//		Properties.TIMELINE_INTERVAL = 3000;
+
+        String fitnessApproach = "branch";
+
+        int timeBudget = 100;
+
+        boolean aor = false;
+        double coverage = TestUtility.evoTestSingleMethod(targetClass,
+                targetMethod, timeBudget, true, aor, cp, fitnessApproach,
+                "generateMOSuite", "MOSUITE", "DynaMOSA");
+
+        assert coverage < 0.05;
+
+    }
+
+    @Test
+    public void testCascadeCall() {
+        Class<?> clazz = ObjectExample.class;
+        String methodName = "test";
+        int parameterNum = 1;
+
+        String targetClass = clazz.getCanonicalName();
+//		Method method = clazz.getMethods()[0];
+        Method method = TestUtility.getTargetMethod(methodName, clazz, parameterNum);
+
+        String targetMethod = method.getName() + MethodUtil.getSignature(method);
+        String cp = "target/test-classes";
+
+        // Properties.LOCAL_SEARCH_RATE = 1;
+//		Properties.DEBUG = true;
+//		Properties.PORT = 8000;
+        Properties.CLIENT_ON_THREAD = true;
+        Properties.STATISTICS_BACKEND = StatisticsBackend.DEBUG;
+
+        Properties.TIMEOUT = 1000;
+//		Properties.TIMELINE_INTERVAL = 3000;
+
+        String fitnessApproach = "branch";
+
+        int timeBudget = 10000;
+
+        boolean aor = true;
+        TestUtility.evoTestSingleMethod(targetClass,
+                targetMethod, timeBudget, true, aor, cp, fitnessApproach,
+                "generateMOSuite", "MOSUITE", "DynaMOSA");
+
+    }
+
+    @Test
+    public void testStudentInterface() {
+        Class<?> clazz = ObjectExample.class;
+        String methodName = "test1";
+        int parameterNum = 2;
+
+        String targetClass = clazz.getCanonicalName();
+//		Method method = clazz.getMethods()[0];
+        Method method = TestUtility.getTargetMethod(methodName, clazz, parameterNum);
+
+        String targetMethod = method.getName() + MethodUtil.getSignature(method);
+        String cp = "target/classes";
+
+        // Properties.LOCAL_SEARCH_RATE = 1;
+//		Properties.DEBUG = true;
+//		Properties.PORT = 8000;
+        Properties.CLIENT_ON_THREAD = true;
+        Properties.STATISTICS_BACKEND = StatisticsBackend.DEBUG;
+
+        Properties.TIMEOUT = 10000000;
+//		Properties.TIMELINE_INTERVAL = 3000;
+
+        String fitnessApproach = "fbranch";
+
+        int timeBudget = 30000;
+        TestUtility.evosuiteFlagBranch(targetClass, targetMethod, cp, timeBudget, true, fitnessApproach);
+    }
+
+    @Test
+    public void testStudentAbstract() {
+        Class<?> clazz = ObjectExample.class;
+        String methodName = "test2";
+        int parameterNum = 2;
+
+        String targetClass = clazz.getCanonicalName();
+//		Method method = clazz.getMethods()[0];
+        Method method = TestUtility.getTargetMethod(methodName, clazz, parameterNum);
+
+        String targetMethod = method.getName() + MethodUtil.getSignature(method);
+        String cp = "target/classes";
+
+        // Properties.LOCAL_SEARCH_RATE = 1;
+//		Properties.DEBUG = true;
+//		Properties.PORT = 8000;
+        Properties.CLIENT_ON_THREAD = true;
+        Properties.STATISTICS_BACKEND = StatisticsBackend.DEBUG;
+
+        Properties.TIMEOUT = 10000000;
+//		Properties.TIMELINE_INTERVAL = 3000;
+
+        String fitnessApproach = "fbranch";
+
+        int timeBudget = 30000;
+        TestUtility.evosuiteFlagBranch(targetClass, targetMethod, cp, timeBudget, true, fitnessApproach);
+    }
+
+    @Test
+    public void testFlagExample1() {
+        ExcelWriter excelWriter;
+
+        Properties.TIMEOUT = 300000000;
+        Properties.RANDOM_SEED = 1606757586999l;
+        Properties.INSTRUMENT_CONTEXT = true;
+        Properties.CRITERION = new Criterion[] { Criterion.BRANCH };
+
+        Properties.APPLY_OBJECT_RULE = false;
+        Properties.APPLY_GRADEINT_ANALYSIS = true;
+        Properties.APPLY_INTERPROCEDURAL_GRAPH_ANALYSIS = true;
+
+        Class<?> clazz = feature.fbranch.example.FlagEffectExample.class;
+        String methodName = "example5";
+        int parameterNum = 2;
+
+        String targetClass = clazz.getCanonicalName();
+//		Method method = clazz.getMethods()[0];
+        Method method = TestUtility.getTargetMethod(methodName, clazz, parameterNum);
+
+        String targetMethod = method.getName() + MethodUtil.getSignature(method);
+        String cp = "target/test-classes";
+
+        // Properties.LOCAL_SEARCH_RATE = 1;
+//		Properties.DEBUG = true;
+//		Properties.PORT = 8000;
+        Properties.CLIENT_ON_THREAD = true;
+        Properties.STATISTICS_BACKEND = StatisticsBackend.DEBUG;
+
+        Properties.TIMEOUT = 1000;
+//		Properties.TIMELINE_INTERVAL = 3000;
+
+        String fitnessApproach = "branch";
+
+        int timeBudget = 10;
+
+        boolean aor = false;
+        TestUtility.evoTestSingleMethod(targetClass,
+                targetMethod, timeBudget, true, aor, cp, fitnessApproach,
+                "generateMOSuite", "MOSUITE", "DynaMOSA");
+
+        excelWriter = new ExcelWriter(evosuite.shell.FileUtils.newFile(Settings.getReportFolder(), "sensitivity_scores.xlsx"));
+        List<String> header = new ArrayList<>();
+        header.add("Class");
+        header.add("Method");
+        header.add("Branch");
+        header.add("Path");
+        header.add("Fitness Value");
+        excelWriter.getSheet("data", header.toArray(new String[header.size()]), 0);
+
+        try {
+            excelWriter.writeSheet("data", SensitivityMutator.data);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+     */
+}
