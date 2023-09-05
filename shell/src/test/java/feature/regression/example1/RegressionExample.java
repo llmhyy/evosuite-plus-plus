2cf1e8e56040113f3764527926c355bb7442a197
Yun Lin <llmhyy@gmail.com>
2023-09-06 02:04:43 +0800

    minor

--- shell/src/test/java/feature/regression/example1/RegressionExample.java
+++ shell/src/test/java/feature/regression/example1/RegressionExample.java
@@ -1,11 +1,13 @@
 package feature.regression.example1;
 
 public class RegressionExample {
-	public void example(int a) {
+	public int example123(int a) {
 		int b = a + 1;
-		if(b >= 1) {
+		if(b < 1) {
 			b++;
 		}
 		
+		return b;
 	}
+	
 }
--- shell/src/test/java/feature/regression/example2/RegressionExample.java
+++ shell/src/test/java/feature/regression/example2/RegressionExample.java
@@ -1,11 +1,14 @@
 package feature.regression.example2;
 
 public class RegressionExample {
-	public void example(int a) {
+	public int example123(int a) {
 		int b = a + 1;
 		if(b > 1) {
 			b++;
 		}
 		
+		return b;
 	}
+	
+	
 }
